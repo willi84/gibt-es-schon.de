c90f3cbda1b636878f4974496af9306b78ad53f3
Robert Willemelis <robert.dev@gmx.de>
2020-09-28 19:55:03 +0200

    fix: path issues

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+
+# deploy
+```
+ng build --prod --base-href="./"
+angular-cli-ghpages --dir=dist/
+```
+
+
+
+
+
+
 [![RealWorld Frontend](https://img.shields.io/badge/realworld-frontend-%23783578.svg)](http://realworld.io)
 [![Build Status](https://travis-ci.org/gothinkster/angular-realworld-example-app.svg?branch=master)](https://travis-ci.org/gothinkster/angular-realworld-example-app)
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,10 @@
 export const environment = {
   production: true,
   title: 'Gibt es schon?',
-  api_url: 'https://conduit.productionready.io/api',
+  api_url: './assets',
   apis: {
     tags: '/tags.json',
     articles: '/articles.json',
   }
-  // apis: {
-  //   tags: '/tags',
-  //   articles: '/assets/articles.json',
-  // }
+  // api_url: 'https://conduit.productionready.io/api'
 };
